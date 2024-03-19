c744b1ae9d4b1bb7fcba96a986faf513dcf8317d
bobby <kajaniew1@gmail.com>
2024-03-19 11:03:12 +0100

    STD

---
+++ app/src/main/java/com/example/std/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.std
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.std.ui.theme.StdTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            setContentView(R.layout.activity_main)
+        }
+
+    }
+
+    fun buttonClick1(view: View) {
+        findViewById<TextView>(R.id.tv3).apply {
+            ;
+            text = context.getString(R.string.hi)
+        }
+    }
+    fun buttonClick2(view: View) {
+        findViewById<TextView>(R.id.tv3).apply {
+            ;
+            text = context.getString(R.string.name)
+        }
+    }
+    fun buttonClick3(view: View) {
+        findViewById<TextView>(R.id.tv3).apply {
+            ;
+            text = context.getString(R.string.me)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StdTheme {
+        Greeting("Android")
+    }
+}
